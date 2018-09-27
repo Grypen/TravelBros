2a3e714123ff27e33d3f15d1d5a42bbce85bb29a
Edvard <edvard.hedlund@gmail.com>
2018-09-27 22:09:35 +0200

    map page

--- TravelBros/MapPage.swift
+++ TravelBros/MapPage.swift
@@ -28,14 +28,22 @@ class MapPage: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
         CLGeocoder().geocodeAddressString(address, completionHandler: {(placemarks, error) in
             if let placemarks = placemarks {
                 let placemark = placemarks[0]
-                if let entryLocation = placemark.location {
+                if let entryMapLocation = placemark.location {
                     
-                    distance = 2*self.myLocation.distance(from: entryLocation)
+                    distance = 2*self.myLocation.distance(from: entryMapLocation)
                     
-                    let region = MKCoordinateRegion.init(center: entryLocation.coordinate, latitudinalMeters: distance, longitudinalMeters: distance)
+                    let region = MKCoordinateRegion.init(center: entryMapLocation.coordinate, latitudinalMeters: distance, longitudinalMeters: distance)
                     self.entryMapView.setRegion(region, animated: false)
                     
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = entryMapLocation.coordinate
+                    let distString = String(format: "%.1f", distance/1000)
+                    annotation.title = "\(self.entryDate)\n\(distString) km"
+                self.entryMapView.addAnnotation(annotation)
                     
+                    self.entryLocation = entryMapLocation
+                    
+                   self.findMyLocation()
                 }
             }
             
@@ -48,5 +56,65 @@ class MapPage: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
         
     }
     
+    func drawDirection() {
+        let startPlacemark = MKPlacemark(coordinate: myLocation.coordinate, addressDictionary: nil)
+        let endPlacemark = MKPlacemark(coordinate: entryLocation.coordinate, addressDictionary: nil)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = MKMapItem(placemark: startPlacemark)
+        directionRequest.destination = MKMapItem(placemark: endPlacemark)
+        
+//        switch routeBtn.selectedSegmentIndex {
+//        case 1:
+//            directionRequest.transportType = MKDirectionsTransportType.walking
+//        default:
+//            directionRequest.transportType = MKDirectionsTransportType.automobile
+//        }
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (routeResponse, routeError) in
+            if let routeResponse = routeResponse  {
+                self.entryMapView.removeOverlays(self.entryMapView.overlays)
+                let route = routeResponse.routes[0]
+                self.entryMapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.lineWidth = 3.0
+        renderer.strokeColor = UIColor.purple
+        renderer.alpha = 0.5
+        return renderer
+    }
+    
+    func findMyLocation() {
+        print("hello location")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations[0]
+        print(newLocation)
+        let distance = myLocation.distance(from: newLocation)
+        if distance > 10 {
+            myLocation = newLocation
+            drawDirection()
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
 }
