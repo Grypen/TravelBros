c8142aac1b5418046eb7defa75304d2ccf1b7698
Edvard <edvard.hedlund@gmail.com>
2018-09-27 15:54:31 +0200

    entry page

--- TravelBros/EntryPage.swift
+++ TravelBros/EntryPage.swift
@@ -10,13 +10,32 @@ import UIKit
 
 class EntryPage: UIViewController {
     
+    
     @IBOutlet weak var dateLabel: UILabel!
-     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
      @IBOutlet weak var entryImage: UIImageView!
     @IBOutlet weak var entryText: UITextView!
     
     var entryID = ""
     
      let entryData = TravelBrosSQL()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        entryData.loadOne(entryId: entryID)
+        setEntryData()
+    }
+    
+    func setEntryData(){
+        dateLabel.text = entryData.oneEntry.date
+        entryText.text = entryData.oneEntry.entry
+        addressLabel.text = entryData.oneEntry.address
+        
+        entryImage.image = entryData.oneEntry.img
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
 }
