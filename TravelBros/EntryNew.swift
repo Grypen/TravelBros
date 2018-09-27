797a3ba573f679097e7f898596a98f9f276ce84f
Damian Wozniak <damian.pwozniak@gmail.com>
2018-09-27 16:40:47 +0200

    Enry

--- TravelBros/EntryNew.swift
+++ TravelBros/EntryNew.swift
@@ -8,4 +8,29 @@
 
 import UIKit
 
-
+class NewEntry: UITableViewController{
+    
+    @IBOutlet weak var datePick: UITextField!
+    
+    private var datePicker: UIDatePicker?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(NewEntry.dateChanged(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(NewEntry.viewTapped(gestureRecognizer:)))
+        
+        datePick.inputView = datePicker
+    }
+    @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer){
+        view.endEditing(true)
+    }
+    @objc func dateChanged(datePicker: UIDatePicker){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        datePick.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
+}
